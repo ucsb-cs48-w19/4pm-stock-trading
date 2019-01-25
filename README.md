d103f8da34abf0cd6660deb369f3dc969ad75b8b
Frank Lee <franklee@umail.ucsb.edu>
2019-01-24 16:29:11 -0800

    Added html hello world

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # 4pm-stock-trading
+## To deploy, install `pip` with the command `$ pip install Flask` and deploy via `$ FLASK_APP=hello.py flask run`. Site is deployed on http://localhost:5000/.
--- hello.py
+++ hello.py
@@ -1,6 +1,7 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
-
-@app.route("/")
-def hello():
-    return "UCSB-CS48 STOCK-TRADING-4PM"
+@app.route('/')
+def home():
+    return render_template('hello.html')
+if __name__ == '__main__':
+    app.run(debug=True)
