2d0f633dec23a874b50cd49f53df1ae333845593
Wesley <wesleychenlu@gmail.com>
2019-01-28 14:42:52 -0800

    changed helloworld to be in HTML

--- hello.py
+++ hello.py
@@ -1,9 +1,7 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
- 
-@app.route("/")
-def hello():
-    return "Hello World!"
- 
-if __name__ == "__main__":
-    app.run()
+@app.route('/')
+def home():
+    return render_template('hello.html')
+if __name__ == '__main__':
+    app.run(debug=True)
