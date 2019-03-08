cca05f179deadbb41db1218d08dc047484bc1c54
weslu1 <wesleychenlu@gmail.com>
2019-03-07 16:36:09 -0800

    Update test_stocks.py

--- test_stocks.py
+++ test_stocks.py
@@ -5,4 +5,10 @@ from stocks import getChart
 
 def test_getChart():
    assert len(getChart("MSFT")) == pytest.approx(254, 10)
+   
+def test_save_stock_tickers():
+   names = []
+   tickers = []
+   save_stock_tickers(names,tickers)
+   assert len(names) == len(tickers)
 
